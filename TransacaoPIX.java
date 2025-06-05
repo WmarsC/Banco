f71a4ee8cb660afa0f596b96c4e1e50a5d031174
Luigi <luigitorlaiokano@gmail.com>
2025-06-04 22:43:01 -0300

    Update TransacaoPIX.java

--- TransacaoPIX.java
+++ TransacaoPIX.java
@@ -2,12 +2,16 @@ import java.time.LocalDateTime;
 
 public class TransacaoPIX {
     private String destinatario;
+    private String remetente;
     private double valor;
+    private String mensagem;
     private LocalDateTime dataHora;
 
-    public TransacaoPIX(String destinatario, double valor) {
+    public TransacaoPIX(String remetente, String destinatario, double valor, String mensagem) {
+        this.remetente = remetente;
         this.destinatario = destinatario;
         this.valor = valor;
+        this.mensagem = mensagem;
         this.dataHora = LocalDateTime.now();
     }
 
@@ -15,8 +19,16 @@ public class TransacaoPIX {
         return destinatario;
     }
 
+    public String getRemetente() {
+        return remetente;
+    }
+
     public double getValor() {
         return valor;
+    }
+
+    public String getMensagem() {
+        return mensagem;
     }
 
     public LocalDateTime getDataHora() {
